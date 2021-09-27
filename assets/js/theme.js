b420c45320cb6eab73cff5950912f112820eb8d5
Neeraj Kumar Das <nkdas91@gmail.com>
2021-09-27 13:57:07 +0530

    Added shape to theme controls

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,13 @@
+function setActiveShape() {
+    $('input[type=radio][name=theme]').each(function () {
+        if (this.checked) {
+            $(this).closest('.m-shape-container').addClass('active');
+        } else {
+            $(this).closest('.m-shape-container').removeClass('active');
+        }
+    });
+}
+
 $(function () {
     if ('default' === $('html').data('theme')) {
         $('#theme-default').attr('checked', 'true');
@@ -7,8 +17,11 @@ $(function () {
         $('#theme-dark').attr('checked', 'true');
     }
 
+    setActiveShape();
+
     $('input[type=radio][name=theme]').change(function() {
         $('html').data('theme', this.value).attr('data-theme', this.value);
         localStorage.setItem('theme', this.value);
+        setActiveShape();
     });
 });
